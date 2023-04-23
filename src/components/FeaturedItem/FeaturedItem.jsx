1193429f5e841bebe2971e55c1fba807101a3cb4
allwinandrews <allwinandrews@gmail.com>
2023-04-23 08:54:34 -0700

    Needs fixing react-hydration-error

---
+++ src/components/FeaturedItem/FeaturedItem.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function FeaturedItem({image}) {
+  return (
+    <div>
+      <div className="featured__item">
+        <div
+          className="featured__item__pic set-bg"
+          
+        >
+          <img src={image}/>
+
+          <ul className="featured__item__pic__hover">
+            <li>
+              <a href="#">
+                <i className="fa fa-heart"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-retweet"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-shopping-cart"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="featured__item__text">
+          <h6>
+            <a href="#">Crab Pool Security</a>
+          </h6>
+          <h5>$30.00</h5>
+        </div>
+      </div>
+    </div>
+  );
+}
