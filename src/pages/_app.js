d2dee667255e9bc2e58fcbc6def966ccb117a7e9
allwinandrews <allwinandrews@gmail.com>
2023-05-28 15:16:25 -0700

    Partial Push

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,23 @@
-import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react";
+
+import "@/styles/globals.css";
 import "../styles/assets/css/bootstrap.min.css";
 import "../styles/assets/css/font-awesome.min.css";
 import "../styles/assets/css/elegant-icons.css";
 import "../styles/assets/css/nice-select.css";
 import "../styles/assets/css/slicknav.min.css";
 import "../styles/assets/css/style.css";
-import '../styles/assets/css/FeaturedItem.css'
-import "../styles/assets/css/signup.css"
-import Context from "../../Context/context"
+import "../styles/assets/css/FeaturedItem.css";
+import "../styles/assets/css/signup.css";
+// import Context from "../../Context/context"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
